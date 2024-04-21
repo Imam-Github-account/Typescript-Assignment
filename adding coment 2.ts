f350b10acfab946f67625c173c5cbc4724c6374f
Imam-Github-account <167673683+Imam-Github-account@users.noreply.github.com>
2024-04-21 13:22:49 +0500

    Add files via upload

---
+++ adding coment 2.ts
@@ -0,0 +1,9 @@
+
+
+// Program written by: IB And Arslii
+
+// Current Date: 20-03-2024
+
+// This program will add two digits.
+
+console.log(2+2);
---
+++ adding coment.ts
@@ -0,0 +1,9 @@
+
+
+// Program written by: IB And Arslii
+
+// Current Date: 20-03-2024
+
+// This program will write Hello World
+
+console.log('Hello World');
